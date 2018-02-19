8e0ac9b268892368b0a5b839f3c98a65d50a609b
simonedaniello <deliterjob@gmail.com>
2018-02-19 17:35:38 +0100

    division entity controllers

---
+++ SDCC/src/main/java/rabbit/controllers/SemaphoreController.java
@@ -0,0 +1,78 @@
+package rabbit.controllers;
+
+import rabbit.entities.Message;
+import rabbit.entities.Semaphore;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Author : Simone D'Aniello
+ * Date :  19-Feb-18.
+ */
+public class SemaphoreController {
+
+    Semaphore semaphore;
+
+    public SemaphoreController(Integer ID){
+        this.semaphore = new Semaphore(ID);
+    }
+
+    private void start(){
+        try {
+            semaphore.getReceive().receiveMessage("localhost", semaphore.getCrossroads());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void getState() {
+        System.out.println("\n------\nID: " + semaphore.getID() + "\nState: " + semaphore.getState()+ "\nBindings: ");
+        for(String c : semaphore.getCrossroads()){
+            System.out.println("\t'" + c + "'");
+        }
+        System.out.println("------\n");
+
+    }
+
+    public void cangeState(Integer state) {
+        semaphore.setState(state);
+        //do things
+    }
+
+
+    public void addCrossroad(String crossroad) {
+        System.out.println("added crossroad : " + crossroad);
+        ArrayList<String> temp;
+        temp = semaphore.getCrossroads();
+        temp.add(crossroad);
+        semaphore.setCrossroads(temp);
+
+        semaphore.getReceive().addBindings(crossroad);
+    }
+
+    public void removeCrossroad(String crossroad) {
+        System.out.println("added crossroad : " + crossroad);
+        ArrayList<String> temp;
+        temp = semaphore.getCrossroads();
+        temp.remove(crossroad);
+        semaphore.setCrossroads(temp);
+
+        semaphore.getReceive().removeBinding(crossroad);
+    }
+
+    public void sendRequest(String queue, String topic) {
+        Message m = new Message(semaphore.getID());
+        Send s1 = new Send(semaphore.getID());
+        try {
+            s1.sendMessage("localhost", m, queue, topic);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ SDCC/src/main/java/rabbit/entities/Semaphore.java
@@ -0,0 +1,52 @@
+package rabbit.entities;
+
+import rabbit.controllers.Receive;
+import rabbit.controllers.Send;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Author : Simone D'Aniello
+ * Date :  15-Feb-18.
+ */
+public class Semaphore{
+
+    private Integer ID;
+    private Integer state; // 0: free, 1: requested, 2: in CS
+    private ArrayList<String> crossroads;
+    private Message m;
+    private Receive r;
+
+    public Semaphore(Integer ID){
+        setID(ID);
+        setState(0);
+        ArrayList<String > c= new ArrayList<String>();
+        setCrossroads(c);
+        Message message  = new Message(ID);
+        setMessage(message);
+        Receive receive = new Receive(this.ID, "traffic");
+        setReceive(receive);
+        try {
+            r.receiveMessage("localhost", this.crossroads);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setID(Integer ID){ this.ID = ID; }
+    public void setState(Integer state) { this.state = state; }
+    public void setCrossroads(ArrayList<String> crossroads){ this.crossroads = crossroads; }
+    public void setMessage (Message m) {this.m = m; }
+    public void setReceive (Receive r){ this.r = r; }
+
+    public Integer getID(){ return this.ID; }
+    public Integer getState() {return this.state; }
+    public ArrayList<String> getCrossroads() {return this.crossroads; }
+    public Message getMessage() { return this.m; }
+    public Receive getReceive() {return this.r; }
+
+
+
+}
